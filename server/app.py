0417e71706a5f11808c96386245a638c015557a1
MohamedHalim <mohamedhalim1998@gmail.com>
2021-05-13 14:52:44 +0200

    ADD: implement simple sever-side database

--- server/app.py
+++ server/app.py
@@ -1,42 +1,62 @@
-from flask import Flask, render_template, session, copy_current_request_context
-from flask_socketio import SocketIO, emit, disconnect, send
+from flask import Flask, render_template, session, request, jsonify
+from flask_socketio import SocketIO, emit
 from threading import Lock
-from apscheduler.schedulers.background import BackgroundScheduler
-import socketio
+from flask_sqlalchemy import SQLAlchemy
+import json
 
 async_mode = None
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'secret!'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
 socket_ = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
-sio = socketio.Client()
+
+
+class Sensor(db.Model):
+    id = db.Column(db.Integer, primary_key=True,
+                   autoincrement=True)
+    temperature = db.Column(db.Float)
+    sensorId = db.Column(db.String(50))
+    vibration = db.Column(db.Float)
+    time = db.Column(db.Integer)
+    dangerLevel = db.Column(db.Integer)
+
+    def serialize(sensor):
+        return {
+            'temperature': sensor.temperature,
+            'sensorId': sensor.sensorId,
+            'vibration': sensor.vibration,
+            'time':  sensor.time,
+            'dangerLevel': sensor.dangerLevel
+        }
 
 
 @app.route('/')
 def index():
-    # init()
     return render_template('index.html', async_mode=socket_.async_mode)
-
-
-def init():
-    print("init")
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(test_job, 'interval', seconds=5, max_instances=100)
-    scheduler.start()
 
 
 @socket_.on('sensor_data')
 def test_sensor_data(message):
-    print(message)
+    list = json.loads(message)['data']
+    for map in list:
+        print(map)
+        sensorId = map['sensorId']
+        temperature = map['temperature']
+        vibration = map['vibration']
+        time = map['time']
+        dangerLevel = map['dangerLevel']
+        sensor = Sensor(sensorId=sensorId, temperature=float(temperature),
+                        vibration=float(vibration), time=time, dangerLevel=dangerLevel)
+        db.session.add(sensor)
+
+    db.session.commit()
     emit('sensor_data', message, broadcast=True)
 
 
-def test_job():
-    socket_.emit('sensor_data', {'data': "send fake"})
-
-
 if __name__ == '__main__':
-    # init()
-    # emit('sensor_data', "data", broadcast=True)
+    db.create_all()
     socket_.run(app, host='0.0.0.0', port=12345, debug=True)
--- server/sender.py
+++ server/sender.py
@@ -3,21 +3,43 @@ import time
 import random
 import json
 
-def on_aaa_response(args):
-    print('sensor_data', args['data'])
+
+SAFE = 0
+WARNING = 1
+DANGEROUS = 2
 
 
 sio = socketio.Client()
 sio.connect("http://192.168.1.2:12345")
-print('my sid is', sio.sid)
-while(1):
-    list = []
-    for i in range(5):
-        list.append({
-            'temperature': random.randint(0, 100),
-            'sensorId': "Device #%d" % i,
-            'vibration': random.randint(0, 100),
-            'time':  int(time.time() * 1000),
-        })
-    sio.emit("sensor_data", json.dumps({'data': list}))
-    time.sleep(1)
+print('conneced to sever session id', sio.sid)
+
+
+@sio.on('disconnect')
+def disconnect():
+    print("Disconnecting ...")
+    exit()
+
+
+if __name__ == '__main__':
+    while(1):
+        list = []
+        for i in range(5):
+            sensor = {
+                'temperature': random.randint(0, 100),
+                'sensorId': "Device #%d" % i,
+                'vibration': random.randint(0, 100),
+                'time':  int(time.time() * 1000),
+            }
+            if(sensor['temperature'] > 80 or sensor['vibration'] > 80):
+                sensor['dangerLevel'] = DANGEROUS
+            elif(sensor['temperature'] > 60 or sensor['vibration'] > 60):
+                sensor['dangerLevel'] = WARNING
+            else:
+                sensor['dangerLevel'] = SAFE
+            list.append(sensor)
+        print(list)
+        try:
+            sio.emit("sensor_data", json.dumps({'data': list}))
+            time.sleep(5)
+        except:
+            exit()
